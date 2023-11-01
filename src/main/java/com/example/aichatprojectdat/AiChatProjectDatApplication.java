34b298684abd62f4632ba6c5ca6c48a9c442db31
realkoder <alex8970@stud.kea.dk>
2023-11-01 12:44:43 +0100

    first commit

---
+++ src/main/java/com/example/aichatprojectdat/AiChatProjectDatApplication.java
@@ -0,0 +1,13 @@
+package com.example.aichatprojectdat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatProjectDatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiChatProjectDatApplication.class, args);
+    }
+
+}
