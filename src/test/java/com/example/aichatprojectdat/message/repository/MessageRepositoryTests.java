3529e08ceff86d2830e67995ac0cfcb73d91b77f
Alex Holmberg <AXTH@ramboll.com>
2023-11-06 12:41:11 +0100

    testing

--- src/test/java/com/example/aichatprojectdat/message/repository/MessageRepositoryTests.java
+++ src/test/java/com/example/aichatprojectdat/message/repository/MessageRepositoryTests.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -23,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@Transactional
 public class MessageRepositoryTests extends AbstractIntegrationTest {
 
     @Autowired
