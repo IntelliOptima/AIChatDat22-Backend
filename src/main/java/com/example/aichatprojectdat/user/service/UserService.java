591ce25ad130fb09ab028e7b572e6038133ac1dd
realkoder <alex8970@stud.kea.dk>
2023-11-09 14:02:17 +0100

    implemented features for user, encoder, decoder, controllers

--- src/main/java/com/example/aichatprojectdat/user/service/UserService.java
+++ src/main/java/com/example/aichatprojectdat/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.aichatprojectdat.user.model.User;
 import com.example.aichatprojectdat.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -13,7 +14,14 @@ public class UserService implements IUserService {
     private final UserRepository userRepository;
 
     @Override
-    public Mono<User> create(User user) {
-        return userRepository.save(user);
+    public Mono<User> createOrReturnExistingUser(User user) {
+        return userRepository.findUserByEmail(user.email())
+                .flatMap(Mono::justOrEmpty)
+                .switchIfEmpty(userRepository.save(user));
+    }
+
+    @Override
+    public Flux<User> findUsersByIdIn(Iterable<Long> userIds) {
+        return userRepository.findUsersByIdIn(userIds);
     }
 }
