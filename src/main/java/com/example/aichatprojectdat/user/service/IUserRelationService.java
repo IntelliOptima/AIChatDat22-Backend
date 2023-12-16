bd8016011941c06d7256332c7f544437f0c044c8
mikk39k1 <mikk39k1@stud.kea.dk>
2023-12-16 14:15:29 +0100

    Create User Relation and display of user Realtion is setup and working

--- src/main/java/com/example/aichatprojectdat/user/service/IUserRelationService.java
+++ src/main/java/com/example/aichatprojectdat/user/service/IUserRelationService.java
@@ -1,4 +1,16 @@
 package com.example.aichatprojectdat.user.service;
 
+import com.example.aichatprojectdat.user.model.User;
+import com.example.aichatprojectdat.user.model.UserRelation;
+import com.example.aichatprojectdat.user.model.UserRelationRequest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface IUserRelationService {
+
+    Flux<UserRelation> getAllUserRelationsFromUserId(Long userId);
+
+    Flux<User> getAllUserFriendsFromUserId(Long userId);
+
+    Mono<User> createUserRelation(UserRelationRequest userRelationRequest);
 }
